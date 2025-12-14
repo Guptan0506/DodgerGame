ff944d8859bea6e189853a26cdf3ba57bf7ce2c0
Navya Gupta <navyag0509@gmail.com>
2025-12-14 18:25:48 -0500

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,1 +1,18 @@
 
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+        setTitle("Dodger");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new GamePanel());
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}

d333acb533283b58f78f45e527a5595b6e341e8e
Navya Gupta <navyag0509@gmail.com>
2025-12-14 18:25:09 -0500

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,1 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    Player player;
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(600, 400));
+        setBackground(Color.BLACK);
+        player = new Player(280, 330);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        player.draw(g);
+    }
+}
 

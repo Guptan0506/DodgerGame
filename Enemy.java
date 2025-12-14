fc5227ab6125e4c70cc6d883b01e6f1e4056ede5
Navya Gupta <navyag0509@gmail.com>
2025-12-14 18:28:11 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,1 +1,36 @@
 
+import java.awt.*;
+import java.util.Random;
+
+public class Enemy {
+
+    int x, y;
+    int width = 40;
+    int height = 40;
+    int speed = 4;
+
+    static Random random = new Random();
+
+    public Enemy(int panelWidth) {
+        reset(panelWidth);
+        y = -random.nextInt(300); // start above the screen
+    }
+
+    public void update(int panelHeight, int panelWidth) {
+        y += speed;
+
+        if (y > panelHeight) {
+            reset(panelWidth);
+        }
+    }
+
+    private void reset(int panelWidth) {
+        x = random.nextInt(panelWidth - width);
+        y = -height;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, width, height);
+    }
+}

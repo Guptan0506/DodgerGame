a5c82e6299b40f85339c039734e528b430446f8c
Navya Gupta <navyag0509@gmail.com>
2025-12-14 18:25:29 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,18 @@
 
+import java.awt.*;
+
+public class Player {
+    int x, y;
+    int width = 40;
+    int height = 40;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(x, y, width, height);
+    }
+}

6e0033407a258fa488646cc8f1ebe8b0c825303e
Navya Gupta <navyag0509@gmail.com>
2025-12-14 18:26:52 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,28 @@
-
 import java.awt.*;
 
 public class Player {
     int x, y;
     int width = 40;
     int height = 40;
+    int speed = 5;   // how fast the player moves
 
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public void moveLeft() {
+        x -= speed;
+        if (x < 0) {
+            x = 0;
+        }
+    }
+
+    public void moveRight(int panelWidth) {
+        x += speed;
+        if (x + width > panelWidth) {
+            x = panelWidth - width;
+        }
     }
 
     public void draw(Graphics g) {
@@ -16,3 +30,4 @@ public class Player {
         g.fillRect(x, y, width, height);
     }
 }
+
